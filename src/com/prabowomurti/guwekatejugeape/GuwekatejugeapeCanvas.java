39354bdec52a1eaed6173a704d3cf0224648d02f
Prabowo Murti <prabowo.murti@gmail.com>
2010-01-14 23:18:31 +0700

    Things done 0. Wrapped text runs well (until I got a bug report, haha) 1. No splashscreen. What can you expect from this midlet?

--- src/com/prabowomurti/guwekatejugeape/GuwekatejugeapeCanvas.java
+++ src/com/prabowomurti/guwekatejugeape/GuwekatejugeapeCanvas.java
@@ -2,7 +2,7 @@ package com.prabowomurti.guwekatejugeape;
 
 /**
  *
- * @author Prabowo Murti
+ * @author Prabowo Murti <prabowo.murti AT gmail.com>
  */
 
 import javax.microedition.lcdui.*;
@@ -13,6 +13,10 @@ public class GuwekatejugeapeCanvas extends Canvas{
 	GuwekatejugeapeMidlet midlet;
 	int w = getWidth();
 	int h = getHeight();
+	final int MARGIN_LEFT = 10,
+			  MARGIN_TOP = 10,
+			  MARGIN_RIGHT = 5;
+
 	int keyCode;
 
 	String number = "";
@@ -77,7 +81,7 @@ public class GuwekatejugeapeCanvas extends Canvas{
 	}
 
 	public void writeNumber (Graphics g, String num) {
-		g.drawString(num, 10, 20, Graphics.LEFT | Graphics.TOP);
+		g.drawString(num, MARGIN_LEFT, MARGIN_TOP, Graphics.LEFT | Graphics.TOP);
 	}
 
 	public void eraseNumber (Graphics g){
@@ -87,6 +91,7 @@ public class GuwekatejugeapeCanvas extends Canvas{
 			writeNumber (g, this.number);
 		}
 	}
+
 	public void clearNumber(Graphics g) {
 		this.number = "";
 		this.numberLength = 0;
@@ -94,12 +99,80 @@ public class GuwekatejugeapeCanvas extends Canvas{
 		createMenu(g);
 	}
 
-	public void writeWords(Graphics g, String w){
-		g.drawString(w, 10, 60, Graphics.LEFT | Graphics.TOP);
+	public void writeWords(Graphics g, String word){
+		if (countWord(word) <= 2){
+			g.drawString(word, MARGIN_LEFT, 50, Graphics.LEFT | Graphics.TOP);
+		}else {
+			wrapText(g, word, MARGIN_LEFT, 50, w-MARGIN_LEFT-MARGIN_RIGHT);
+		}
+	}
+
+	public void wrapText(Graphics g, String words, int xPosition, int yPosition, int lineWidth){
+		int xPositionOriginal = xPosition;
+		int wordWidth;
+		int spaceLeftAtThisLine = lineWidth;
+		int numOfWords = countWord(words);
+		String[] splitWords = new String[numOfWords];
+
+		//split words into splitWords (God, this line IS EXPENSIVE!)
+		splitWords = split(words);
+
+		for (int i=0 ; i<numOfWords ; i++){
+			wordWidth = g.getFont().stringWidth(splitWords[i]);
+			if (wordWidth <= spaceLeftAtThisLine){
+				//write on the same line
+				g.drawString(splitWords[i], xPosition, yPosition, Graphics.LEFT | Graphics.TOP);
+				xPosition += wordWidth;
+				spaceLeftAtThisLine = spaceLeftAtThisLine - wordWidth;
+			}else {
+				//write on the next line
+				yPosition = yPosition + g.getFont().getHeight();//
+				xPosition = xPositionOriginal;
+				g.drawString(splitWords[i], xPosition, yPosition, Graphics.LEFT | Graphics.TOP);
+				xPosition += wordWidth;
+				spaceLeftAtThisLine = lineWidth - wordWidth;
+			}
+		}
+	}
+
+	public int countWord(String word){
+		if (word.trim().length() == 0)
+			return 0;
+		if (word.trim().length() <= 2)
+			return 1;
+
+		//I waste 55 minutes just because I forget to change this var's value to 0. F***...
+		int count = 1;//always get a space at the beginning of the word
+		word = " " + word.trim();
+		int searchStringPos = word.indexOf(" ", 1), startPos = 1;
+		while (searchStringPos != -1){
+			count ++;
+			startPos = searchStringPos + 1; //1 == length of " "
+			searchStringPos = word.indexOf(" ", startPos);
+		}
+		return count;
+	}
+
+	public String[] split(String word){
+		int count = countWord(word);
+		String[] result = new String[count];
+		word = " " + word.trim();
+		int startPos = 1;
+		int searchPos;
+
+		for (int i=0; i<count; i++){
+			searchPos = word.indexOf(" ", startPos);
+			result[i] = word.substring(startPos-1, (searchPos!=-1?searchPos:word.length()));
+			startPos = searchPos + 1;
+		}
+
+		return result;
 	}
 	
 	public void clearScreen(Graphics g) {
-		g.setGrayScale(221);//#CCCCCC
+		//g.setGrayScale(221);//#CCCCCC
+		//g.setColor(85, 50, 133);//#553285
+		g.setColor(151, 104, 209);//#9768D1
 		g.fillRect (0, 0, w, h);
 	}
 
